479a5b0a277e084e8d85422f94cc24319ffefe69
nishp77 <35785259+nishp77@users.noreply.github.com>
2018-05-20 21:23:01 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,77 +1,131 @@
 let ball;
-let SPACE = 50; // Spacing between Steps
+let SPACE=50;
 let steps = [];
+let score = 0;
+let player;
 
-function setup() {
-  createCanvas(350, 600);
-  ball = new Ball();
-  let button = createButton("Play Again");
-  button.mouseClicked(reSketch);
-}
+let database;
+let ref;
+let data;
 
-function draw() {
-  background(0);
+function setup() 
+{
+  createCanvas(350,600);
+  ball=new Ball();
+  player=name();
+  let button=createButton("Play Again");
+  button.mouseClicked(reloadPage);
 
-  if (keyIsDown(LEFT_ARROW)) {
-    ball.left();
-  }
+    var config = {
+        apiKey: "AIzaSyBmejIO7Oxq-4JR6Mx1vDwZzxRoNZPEWkY",
+        authDomain: "rapid-roll-c7e9e.firebaseapp.com",
+        databaseURL: "https://rapid-roll-c7e9e.firebaseio.com",
+        projectId: "rapid-roll-c7e9e",
+        storageBucket: "",
+        messagingSenderId: "469826688525"
+    };
+    firebase.initializeApp(config);
+    console.log(firebase);
 
-  if (keyIsDown(RIGHT_ARROW)) {
-    ball.right();
-  }
-
-  ball.update();
-  ball.show();
-
-  // Every 10 frames generate new Bar/Spike
-  if (frameCount % 10 === 0) {
-    genRandomSteps();
-  }
-
-  // Update/Draw every Step
-  for (let i = 0; i < steps.length; i++) {
-    steps[i].show();
-    steps[i].update();
-    ball.checkStep(steps[i]);
-  }
-
-  checkGameOver();
-}
-
-function reSketch() {
-  redraw();
-}
-
-function genRandomSteps() {
-  // Generate a new Spike or Step
-  if (Math.random() >= 0.5) {
-    addObject(true); // Add Spike
-  } else {
-    addObject(false); // Add Bar
-  }
+    database = firebase.database();
+    ref = database.ref('Scores');
 
 }
 
-function addObject(isSpike) {
-  let leftY;
+function draw()
+{
+  	background(0);
 
-  // Calculate height of new Spike
-  if (steps.length === 0) {
-    leftY = SPACE;
-  } else {
-    leftY = ((steps.length * SPACE) + SPACE);
-  }
+    if (keyIsDown(LEFT_ARROW))
+    {
+        ball.left();
+    }
 
-  let leftX = random(width - (WIDTH * 2));
+    if (keyIsDown(RIGHT_ARROW))
+    {
+        ball.right();
+    }
 
-  // Create a new Object with these attributes
-  steps.push(new Step(leftX, leftY, isSpike));
+    ball.update();
+    ball.show();
+
+    if(frameCount % 10 ===0)
+  	{
+  		genRandomSteps();
+
+  	}
+
+  	for (let i = 0; i < steps.length; i++)
+  	{
+  		steps[i].show();
+  		steps[i].update();
+        ball.onStep(steps[i]);
+  	}
+
+    checkGameOver();
+
+
 }
 
-function checkGameOver() {
-  // If ball is outside of Window
-  if (ball.y > height || ball.y < 0) {
-    console.log("Game over");
-    noLoop();
-  }
+function name()
+{
+    let person = prompt("Please enter your name", "");
+	return person;
+
 }
+
+function reloadPage()
+{
+    window.location.reload();
+}
+
+function genRandomSteps()
+{
+    if (Math.random() >= 0.5)
+    {
+        addObject(true); // Add Spike
+    }
+    else
+	{
+		addObject(false); // Add Bar
+    }
+
+}
+
+function addObject(isSpike)
+{
+	let leftY;
+    if (steps.length === 0) {
+        leftY = SPACE;
+    } else {
+        leftY = ((steps.length * SPACE) + SPACE);
+    }
+
+    let leftX = random(width - (WIDTH * 2));
+
+    // Create a new Object with these attributes
+    steps.push(new Steps(leftX, leftY, isSpike));
+}
+
+function checkGameOver()
+{
+
+    if(ball.y>height|| ball.y <0)
+	{
+		console.log("Game over");
+		console.log(score);
+		noLoop();
+		submitData();
+	}
+}
+
+function submitData()
+{
+    data= {
+        name: player,
+        scores: score
+    };
+    ref.push(data);
+}
+
+
